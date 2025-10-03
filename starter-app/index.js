f6872f723fd062785b3825e98e20b7e3c044b418
Monark Sharma <156456877+monark04@users.noreply.github.com>
2025-10-03 19:51:29 +0530

    Added /monark-hello-route

--- starter-app/index.js
+++ starter-app/index.js
@@ -5,5 +5,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('🚀 Hello from Node.js app deployed via Jenkins + Docker!');
 });
+app.get('/hello', (req, res) => {
+  res.send('Hello, from Monark Sharma. This is my DevOps assignment!');
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
